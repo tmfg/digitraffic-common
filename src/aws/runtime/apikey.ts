3071b30ffaf8f226733ddf014e9bcae9533ea55a
Jani Haakana <jani.k.haakana@solita.fi>
2022-09-28 16:02:51 +0300

    Add files from digitraffic-cdk/digitraffic-common

---
+++ src/aws/runtime/apikey.ts
@@ -0,0 +1,9 @@
+import {APIGateway} from "aws-sdk";
+
+export function getApiKeyFromAPIGateway(keyId: string): Promise<APIGateway.Types.ApiKey> {
+    const agw = new APIGateway();
+    return agw.getApiKey({
+        apiKey: keyId,
+        includeValue: true,
+    }).promise();
+}
