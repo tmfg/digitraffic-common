12b853cfe17d89927a0017239b85ecf0c8d91248
Teijo Rönkä <teijo.ronka@solita.fi>
2022-10-31 17:35:52 +0200

    fix MediaType.APPLICATION_JSON

--- src/aws/infra/stack/rest_apis.ts
+++ src/aws/infra/stack/rest_apis.ts
@@ -4,48 +4,70 @@ import {
     GatewayResponse,
     ResponseType,
     EndpointType,
-    RestApiProps, JsonSchema, Model, CfnDocumentationPart, Resource,
-} from 'aws-cdk-lib/aws-apigateway';
-import {PolicyDocument, PolicyStatement, Effect, AnyPrincipal} from 'aws-cdk-lib/aws-iam';
-import {Construct} from "constructs";
-import {DigitrafficStack} from "./stack";
-import {createDefaultUsagePlan, createUsagePlan} from "../usage-plans";
-import {ModelWithReference} from "../../types/model-with-reference";
-import {getModelReference} from "../../../utils/api-model";
-import {MediaType} from "../../types/mediatypes";
-import {DocumentationPart, DocumentationProperties} from "../documentation";
+    RestApiProps,
+    JsonSchema,
+    Model,
+    CfnDocumentationPart,
+    Resource,
+} from "aws-cdk-lib/aws-apigateway";
+import {
+    PolicyDocument,
+    PolicyStatement,
+    Effect,
+    AnyPrincipal,
+} from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+import { DigitrafficStack } from "./stack";
+import { createDefaultUsagePlan, createUsagePlan } from "../usage-plans";
+import { ModelWithReference } from "../../types/model-with-reference";
+import { getModelReference } from "../../../utils/api-model";
+import { MediaType } from "../../types/mediatypes";
+import { DocumentationPart, DocumentationProperties } from "../documentation";
 
-import R = require('ramda');
+import R = require("ramda");
 
 export class DigitrafficRestApi extends RestApi {
     readonly apiKeyIds: string[];
     readonly enableDocumentation: boolean;
 
     constructor(
-        stack: DigitrafficStack, apiId: string, apiName: string, allowFromIpAddresses?: string[] | undefined, config?: Partial<RestApiProps>,
+        stack: DigitrafficStack,
+        apiId: string,
+        apiName: string,
+        allowFromIpAddresses?: string[] | undefined,
+        config?: Partial<RestApiProps>
     ) {
-        const policyDocument = allowFromIpAddresses == null ? createDefaultPolicyDocument() : createIpRestrictionPolicyDocument(allowFromIpAddresses);
+        const policyDocument =
+            allowFromIpAddresses == null
+                ? createDefaultPolicyDocument()
+                : createIpRestrictionPolicyDocument(allowFromIpAddresses);
 
         // override default config with given extra config
-        const apiConfig = {...{
-            deployOptions: {
-                loggingLevel: MethodLoggingLevel.ERROR,
+        const apiConfig = {
+            ...{
+                deployOptions: {
+                    loggingLevel: MethodLoggingLevel.ERROR,
+                },
+                restApiName: apiName,
+                endpointTypes: [EndpointType.REGIONAL],
+                policy: policyDocument,
             },
-            restApiName: apiName,
-            endpointTypes: [EndpointType.REGIONAL],
-            policy: policyDocument,
-        }, ...config};
+            ...config,
+        };
 
         super(stack, apiId, apiConfig);
 
         this.apiKeyIds = [];
-        this.enableDocumentation = stack.configuration.stackFeatures?.enableDocumentation ?? true;
+        this.enableDocumentation =
+            stack.configuration.stackFeatures?.enableDocumentation ?? true;
 
         add404Support(this, stack);
     }
 
     hostname(): string {
-        return `${this.restApiId}.execute-api.${(this.stack as DigitrafficStack).region}.amazonaws.com`;
+        return `${this.restApiId}.execute-api.${
+            (this.stack as DigitrafficStack).region
+        }.amazonaws.com`;
     }
 
     createUsagePlan(apiKeyId: string, apiKeyName: string): string {
@@ -65,32 +87,44 @@ export class DigitrafficRestApi extends RestApi {
     }
 
     addJsonModel(modelName: string, schema: JsonSchema) {
-        return this.getModelWithReference(this.addModel(modelName, {
-            contentType: MediaType.APPLICATION_JSON,
-            modelName,
-            schema,
-        }));
+        return this.getModelWithReference(
+            this.addModel(modelName, {
+                contentType: MediaType.APPLICATION_JSON,
+                modelName,
+                schema,
+            })
+        );
     }
 
     addCSVModel(modelName: string) {
-        return this.getModelWithReference(this.addModel(modelName, {
-            contentType: MediaType.TEXT_CSV,
-            modelName,
-            schema: {},
-        }));
+        return this.getModelWithReference(
+            this.addModel(modelName, {
+                contentType: MediaType.TEXT_CSV,
+                modelName,
+                schema: {},
+            })
+        );
     }
 
     private getModelWithReference(model: Model): ModelWithReference {
-        return R.assoc('modelReference', getModelReference(model.modelId, this.restApiId), model) as ModelWithReference;
+        return R.assoc(
+            "modelReference",
+            getModelReference(model.modelId, this.restApiId),
+            model
+        ) as ModelWithReference;
     }
 
     private addDocumentationPart(
-        resource: Resource, parameterName: string, resourceName: string, type: string, properties: DocumentationProperties,
+        resource: Resource,
+        parameterName: string,
+        resourceName: string,
+        type: string,
+        properties: DocumentationProperties
     ) {
         const location: CfnDocumentationPart.LocationProperty = {
             type,
             path: resource.path,
-            name: type !== 'METHOD' ? parameterName : undefined,
+            name: type !== "METHOD" ? parameterName : undefined,
         };
 
         new CfnDocumentationPart(this.stack, resourceName, {
@@ -100,11 +134,20 @@ export class DigitrafficRestApi extends RestApi {
         });
     }
 
-    documentResource(resource: Resource, ...documentationPart: DocumentationPart[]) {
-        if(this.enableDocumentation) {
-            documentationPart.forEach(dp => this.addDocumentationPart(
-                resource, dp.parameterName, `${resource.path}.${dp.parameterName}.Documentation`, dp.type, dp.documentationProperties,
-            ));
+    documentResource(
+        resource: Resource,
+        ...documentationPart: DocumentationPart[]
+    ) {
+        if (this.enableDocumentation) {
+            documentationPart.forEach((dp) =>
+                this.addDocumentationPart(
+                    resource,
+                    dp.parameterName,
+                    `${resource.path}.${dp.parameterName}.Documentation`,
+                    dp.type,
+                    dp.documentationProperties
+                )
+            );
         } else {
             console.info("Skipping documentation for %s", resource.path);
         }
@@ -119,25 +162,33 @@ export class DigitrafficRestApi extends RestApi {
  * @param stack Construct
  */
 export function add404Support(restApi: RestApi, stack: Construct) {
-    new GatewayResponse(stack, `MissingAuthenticationTokenResponse-${restApi.restApiName}`, {
-        restApi,
-        type: ResponseType.MISSING_AUTHENTICATION_TOKEN,
-        statusCode: '404',
-        templates: {
-            'application/json': '{"message": "Not found"}',
-        },
-    });
+    new GatewayResponse(
+        stack,
+        `MissingAuthenticationTokenResponse-${restApi.restApiName}`,
+        {
+            restApi,
+            type: ResponseType.MISSING_AUTHENTICATION_TOKEN,
+            statusCode: "404",
+            templates: {
+                [MediaType.APPLICATION_JSON]: '{"message": "Not found"}',
+            },
+        }
+    );
 }
 
 export function add401Support(restApi: RestApi, stack: Construct) {
-    new GatewayResponse(stack, `AuthenticationFailedResponse-${restApi.restApiName}`, {
-        restApi,
-        type: ResponseType.UNAUTHORIZED,
-        statusCode: "401",
-        responseHeaders: {
-            'WWW-Authenticate': "'Basic'",
-        },
-    });
+    new GatewayResponse(
+        stack,
+        `AuthenticationFailedResponse-${restApi.restApiName}`,
+        {
+            restApi,
+            type: ResponseType.UNAUTHORIZED,
+            statusCode: "401",
+            responseHeaders: {
+                "WWW-Authenticate": "'Basic'",
+            },
+        }
+    );
 }
 
 /**
@@ -149,23 +200,36 @@ export function add401Support(restApi: RestApi, stack: Construct) {
  * @param restApi RestApi
  * @param stack Construct
  */
-export function setReturnCodeForMissingAuthenticationToken(returnCode: number,
+export function setReturnCodeForMissingAuthenticationToken(
+    returnCode: number,
     message: string,
     restApi: RestApi,
-    stack: Construct) {
-
-    new GatewayResponse(stack, `MissingAuthenticationTokenResponse-${restApi.restApiName}`, {
-        restApi,
-        type: ResponseType.MISSING_AUTHENTICATION_TOKEN,
-        statusCode: `${returnCode}`,
-        templates: {
-            'application/json': `{"message": ${message}}`,
-        },
-    });
+    stack: Construct
+) {
+    new GatewayResponse(
+        stack,
+        `MissingAuthenticationTokenResponse-${restApi.restApiName}`,
+        {
+            restApi,
+            type: ResponseType.MISSING_AUTHENTICATION_TOKEN,
+            statusCode: `${returnCode}`,
+            templates: {
+                [MediaType.APPLICATION_JSON]: `{"message": ${message}}`,
+            },
+        }
+    );
 }
 
-export function createRestApi(stack: Construct, apiId: string, apiName: string, allowFromIpAddresses?: string[] | undefined): RestApi {
-    const policyDocument = allowFromIpAddresses == null ? createDefaultPolicyDocument() : createIpRestrictionPolicyDocument(allowFromIpAddresses);
+export function createRestApi(
+    stack: Construct,
+    apiId: string,
+    apiName: string,
+    allowFromIpAddresses?: string[] | undefined
+): RestApi {
+    const policyDocument =
+        allowFromIpAddresses == null
+            ? createDefaultPolicyDocument()
+            : createIpRestrictionPolicyDocument(allowFromIpAddresses);
     const restApi = new RestApi(stack, apiId, {
         deployOptions: {
             loggingLevel: MethodLoggingLevel.ERROR,
@@ -183,40 +247,29 @@ export function createDefaultPolicyDocument() {
         statements: [
             new PolicyStatement({
                 effect: Effect.ALLOW,
-                actions: [
-                    "execute-api:Invoke",
-                ],
-                resources: [
-                    "*",
-                ],
-                principals: [
-                    new AnyPrincipal(),
-                ],
+                actions: ["execute-api:Invoke"],
+                resources: ["*"],
+                principals: [new AnyPrincipal()],
             }),
         ],
     });
 }
 
-
-export function createIpRestrictionPolicyDocument(allowFromIpAddresses: string[]): PolicyDocument {
+export function createIpRestrictionPolicyDocument(
+    allowFromIpAddresses: string[]
+): PolicyDocument {
     return new PolicyDocument({
         statements: [
             new PolicyStatement({
                 effect: Effect.ALLOW,
                 conditions: {
-                    "IpAddress": {
+                    IpAddress: {
                         "aws:SourceIp": allowFromIpAddresses,
                     },
                 },
-                actions: [
-                    "execute-api:Invoke",
-                ],
-                resources: [
-                    "*",
-                ],
-                principals: [
-                    new AnyPrincipal(),
-                ],
+                actions: ["execute-api:Invoke"],
+                resources: ["*"],
+                principals: [new AnyPrincipal()],
             }),
         ],
     });
