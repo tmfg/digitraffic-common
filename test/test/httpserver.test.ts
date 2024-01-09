2bd16a491d293c5c445853b30e21e402d87ec929
Joonas-M-S <joonas.mikkonen@solita.fi>
2024-01-09 16:40:59 +0200

    Remove describe block as the tests just freeze inside of it.

--- test/test/httpserver.test.ts
+++ test/test/httpserver.test.ts
@@ -14,115 +14,113 @@ const DEFAULT_PROPS: ListenProperties = {
     "/": () => "",
 };
 
-describe("TestHttpServer - test", () => {
-    async function withServer(
-        fn: (server: TestHttpServer) => unknown,
-        props: ListenProperties = DEFAULT_PROPS,
-        statusCode = 200
-    ) {
-        const server = new TestHttpServer();
+async function withServer(
+    fn: (server: TestHttpServer) => unknown,
+    props: ListenProperties = DEFAULT_PROPS,
+    statusCode = 200,
+) {
+    const server = new TestHttpServer();
 
-        server.listen(PORT, props, false, statusCode);
+    server.listen(PORT, props, false, statusCode);
 
-        try {
-            await fn(server);
-        } finally {
-            server.close();
-        }
+    try {
+        await fn(server);
+    } finally {
+        await server.close();
     }
+}
 
-    function sendGetRequest(path = DEFAULT_PATH): Promise<IncomingMessage> {
-        return sendRequest("GET", path);
-    }
+function sendGetRequest(path = DEFAULT_PATH): Promise<IncomingMessage> {
+    return sendRequest("GET", path);
+}
 
-    function sendPostRequest(
-        path = DEFAULT_PATH,
-        body: string
-    ): Promise<IncomingMessage> {
-        return sendRequest("POST", path, body);
-    }
+function sendPostRequest(
+    path = DEFAULT_PATH,
+    body: string,
+): Promise<IncomingMessage> {
+    return sendRequest("POST", path, body);
+}
 
-    function sendRequest(
-        method: string,
-        path: string,
-        body?: string
-    ): Promise<IncomingMessage> {
-        return new Promise((resolve, reject) => {
-            const request = http.request(
-                {
-                    path,
-                    port: PORT,
-                    method,
-                },
-                (response: IncomingMessage) => {
-                    response.on("data", () => {
-                        // do nothing
-                    });
-
-                    //the whole response has been received, so we just print it out here
-                    response.on("end", () => {
-                        resolve(response);
-                    });
-
-                    response.on("error", (error: Error) => {
-                        reject(error);
-                    });
-                }
-            );
-
-            if (method === "POST") {
-                request.write(body);
-            }
-            request.end();
-        });
-    }
-
-    test("no calls", () => {
-        return withServer((server: TestHttpServer) => {
-            expect(server.getCallCount()).toEqual(0);
-        });
-    });
-
-    test("one get", async () => {
-        await withServer(async (server: TestHttpServer) => {
-            await sendGetRequest();
-
-            expect(server.getCallCount()).toEqual(1);
-        });
-    });
-
-    test("one get - no MATCH", async () => {
-        await withServer(async (server: TestHttpServer) => {
-            const response = await sendGetRequest("/no-match");
-
-            expect(server.getCallCount()).toEqual(1);
-            expect(server.getRequestBody(0)).toEqual(ERROR_NO_MATCH);
-            expect(response.statusCode).toEqual(ERRORCODE_NOT_FOUND);
-        });
-    });
-
-    test("get - error 405", async () => {
-        const ERROR_CODE = 405;
-
-        await withServer(
-            async (server: TestHttpServer) => {
-                const response = await sendGetRequest();
-
-                expect(server.getCallCount()).toEqual(1);
-                expect(response.statusCode).toEqual(ERROR_CODE);
+function sendRequest(
+    method: string,
+    path: string,
+    body?: string,
+): Promise<IncomingMessage> {
+    return new Promise((resolve, reject) => {
+        const request = http.request(
+            {
+                path,
+                port: PORT,
+                method,
             },
-            DEFAULT_PROPS,
-            ERROR_CODE
-        );
-    });
+            (response: IncomingMessage) => {
+                response.on("data", () => {
+                    // do nothing
+                });
 
-    test("one post", async () => {
-        await withServer(async (server: TestHttpServer) => {
-            const testBody = "Testing123!";
-            await sendPostRequest(DEFAULT_PATH, testBody);
+                //the whole response has been received, so we just print it out here
+                response.on("end", () => {
+                    resolve(response);
+                });
+
+                response.on("error", (error: Error) => {
+                    reject(error);
+                });
+            },
+        );
+
+        if (method === "POST") {
+            request.write(body);
+        }
+        request.end();
+    });
+}
+
+test("no calls", () => {
+    return withServer((server: TestHttpServer) => {
+        expect(server.getCallCount()).toEqual(0);
+    });
+});
+
+test("one get", async () => {
+    await withServer(async (server: TestHttpServer) => {
+        await sendGetRequest();
+
+        expect(server.getCallCount()).toEqual(1);
+    });
+});
+
+test("one get - no MATCH", async () => {
+    await withServer(async (server: TestHttpServer) => {
+        const response = await sendGetRequest("/no-match");
+
+        expect(server.getCallCount()).toEqual(1);
+        expect(server.getRequestBody(0)).toEqual(ERROR_NO_MATCH);
+        expect(response.statusCode).toEqual(ERRORCODE_NOT_FOUND);
+    });
+});
+
+test("get - error 405", async () => {
+    const ERROR_CODE = 405;
+
+    await withServer(
+        async (server: TestHttpServer) => {
+            const response = await sendGetRequest();
 
             expect(server.getCallCount()).toEqual(1);
-            expect(server.getRequestBody(0)).toEqual(testBody);
-        });
+            expect(response.statusCode).toEqual(ERROR_CODE);
+        },
+        DEFAULT_PROPS,
+        ERROR_CODE,
+    );
+});
+
+test("one post", async () => {
+    await withServer(async (server: TestHttpServer) => {
+        const testBody = "Testing123!";
+        await sendPostRequest(DEFAULT_PATH, testBody);
+
+        expect(server.getCallCount()).toEqual(1);
+        expect(server.getRequestBody(0)).toEqual(testBody);
     });
 });
