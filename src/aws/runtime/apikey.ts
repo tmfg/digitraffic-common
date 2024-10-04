40d2431f663219ea9b64e2601b55cc76258fbe51
teijosol <teijo.ronka@solita.fi>
2024-10-04 09:25:21 +0300

    add model to StaticIntegration

--- src/aws/runtime/apikey.ts
+++ src/aws/runtime/apikey.ts
@@ -1,6 +1,3 @@
-//import { APIGatewayClient, GetApiKeyCommand } from "@aws-sdk/client-api-gateway";
-//import type { GetApiKeyCommandOutput } from "@aws-sdk/client-api-gateway";
-
 import { APIGateway } from "aws-sdk";
 import type { ApiKey } from "aws-sdk/clients/apigateway.js";
 
@@ -11,14 +8,5 @@ export async function getApiKeyFromAPIGateway(keyId: string): Promise<ApiKey> {
         .getApiKey({
             apiKey: keyId,
             includeValue: true,
-        })
-        .promise();
-    /*
-    const client = new APIGatewayClient();
-    const command = new GetApiKeyCommand({
-        apiKey: keyId,
-        includeValue: true,
-    });
-
-    return client.send(command);*/
+        }).promise();
 }
