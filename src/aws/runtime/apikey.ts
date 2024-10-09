0a9294730c516cff49e63bcd7af6e9bd7a74cd9d
teijosol <teijo.ronka@solita.fi>
2024-10-09 14:29:05 +0300

    DPO-1785 sdk3, new puppeteer

--- src/aws/runtime/apikey.ts
+++ src/aws/runtime/apikey.ts
@@ -1,12 +1,14 @@
-import { APIGateway } from "aws-sdk";
-import type { ApiKey } from "aws-sdk/clients/apigateway.js";
+import { APIGatewayClient, GetApiKeyCommand, type GetApiKeyCommandOutput } from "@aws-sdk/client-api-gateway";
+import { FetchHttpHandler } from "@smithy/fetch-http-handler"
 
-export async function getApiKeyFromAPIGateway(keyId: string): Promise<ApiKey> {
-    const ag = new APIGateway();
+export async function getApiKeyFromAPIGateway(keyId: string): Promise<GetApiKeyCommandOutput> {
+    const client = new APIGatewayClient({
+        requestHandler: new FetchHttpHandler()
+    });
+    const command = new GetApiKeyCommand({
+        apiKey: keyId,
+        includeValue: true
+    });
 
-    return ag
-        .getApiKey({
-            apiKey: keyId,
-            includeValue: true,
-        }).promise();
+    return (await client.send(command));
 }
