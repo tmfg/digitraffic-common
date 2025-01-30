6a6792eb9a37e8cc7de0a6f9fa922564c46695ec
jouniso <jouniso@users.noreply.github.com>
2025-01-30 13:40:37 +0100

    DPO-3017 Deno fmt formatterin käyttöönotto prettierin tilalle - all in

--- src/types/traffictype.ts
+++ src/types/traffictype.ts
@@ -1,9 +1,9 @@
 export enum TrafficType {
-    ROAD = "Road",
-    MARINE = "Marine",
-    RAIL = "Rail",
-    AVIATION = "Aviation",
-    AFIR = "Afir",
-    MCP = "MCP",
-    OTHER = "Other",
+  ROAD = "Road",
+  MARINE = "Marine",
+  RAIL = "Rail",
+  AVIATION = "Aviation",
+  AFIR = "Afir",
+  MCP = "MCP",
+  OTHER = "Other",
 }
