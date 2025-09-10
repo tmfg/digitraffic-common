b74af7cece93254e8c30cc3acc490287c13db29b
Joonas-M-S <joonas.mikkonen@solita.fi>
2025-09-10 10:12:18 +0300

    Add support for a regular stack to createLambdaLogGroup function

--- src/aws/infra/stack/lambda-log-group.ts
+++ src/aws/infra/stack/lambda-log-group.ts
@@ -1,13 +1,68 @@
 import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
 import type { DigitrafficStack } from "./stack.js";
+import type { Stack } from "aws-cdk-lib";
+
+export interface CreateLambdaLogGroupParams {
+  functionName: string;
+  retention?: RetentionDays;
+}
+
+export interface CreateLambdaLogGroupParamsForStack
+  extends CreateLambdaLogGroupParams {
+  stack: Stack;
+  shortName: string;
+}
+
+export interface CreateLambdaLogGroupParamsForDigitrafficStack
+  extends CreateLambdaLogGroupParams {
+  stack: DigitrafficStack;
+}
+
+function isCreateLambdaLogGroupParamsForDigitrafficStack(
+  params:
+    | CreateLambdaLogGroupParamsForStack
+    | CreateLambdaLogGroupParamsForDigitrafficStack,
+): params is CreateLambdaLogGroupParamsForDigitrafficStack {
+  return !!("configuration" in params.stack &&
+    (params.stack as DigitrafficStack)?.configuration?.shortName);
+}
 
 export function createLambdaLogGroup(
-  stack: DigitrafficStack,
-  functionName: string,
-  retention: RetentionDays = RetentionDays.ONE_YEAR,
-) {
-  return new LogGroup(stack, `${functionName}-LogGroup`, {
-    logGroupName: `/${stack.configuration.shortName}/lambda/${functionName}`,
+  params:
+    | CreateLambdaLogGroupParamsForStack
+    | CreateLambdaLogGroupParamsForDigitrafficStack,
+): LogGroup {
+  const _createLambdaLogGroup = (
+    stack: Stack,
+    functionName: string,
+    shortName: string,
+    retention: RetentionDays,
+  ) => {
+    return new LogGroup(stack, `${functionName}-LogGroup`, {
+      logGroupName: `/${shortName}/lambda/${functionName}`,
+      retention,
+    });
+  };
+
+  const getShortName = (
+    params:
+      | CreateLambdaLogGroupParamsForStack
+      | CreateLambdaLogGroupParamsForDigitrafficStack,
+  ) => {
+    if (isCreateLambdaLogGroupParamsForDigitrafficStack(params)) {
+      return params.stack.configuration.shortName;
+    } else {
+      return params.shortName;
+    }
+  };
+
+  const { functionName, retention = RetentionDays.ONE_YEAR } = params;
+  const shortName = getShortName(params);
+
+  return _createLambdaLogGroup(
+    params.stack,
+    functionName,
+    shortName,
     retention,
-  });
+  );
 }
