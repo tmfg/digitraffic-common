c36e347f652ef7ccc191853cb57f7e56dafa9163
Ilari Junnonen <ilari.junnonen@solita.fi>
2026-01-09 16:39:34 +0200

    DPO-3623 add zod utils

---
+++ src/utils/zod-utils.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+/**
+ * Functions for validating and parsing e.g. path and query parameters with zod.
+ * The value of the message parameter will appear in the zod error object resulting from failed validation.
+ *
+ * Usage:
+ * const mySchema = z
+ *  .object({
+ *    myParameter: z
+ *      .zStringToDate("Invalid value of myParameter")
+ *      .optional()
+ *  });
+ *
+ */
+
+export function zStringToNumber(message?: string) {
+  return z
+    .string()
+    .transform(Number)
+    .refine((vesselId) => !Number.isNaN(vesselId), {
+      message: message ? message : "Not a number",
+    });
+}
+
+export function zStringToDate(message?: string) {
+  return z
+    .string()
+    .transform((val) => (val ? new Date(val) : undefined))
+    .refine((date) => !date || !Number.isNaN(date.getTime()), {
+      message: message ? message : "Invalid date format",
+    });
+}
